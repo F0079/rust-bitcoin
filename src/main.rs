c431a208416d216f35f79ce94aa2cfc8a2d8b9af
F0079 <dev@f0079.de>
2025-07-05 15:48:48 +0200

    print date and time

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use chrono::{DateTime, Utc};
 use reqwest;
 use serde_json::Value;
+use std::time::SystemTime;
 
 fn main() {
+    let time: SystemTime = SystemTime::now();
+    let dt: DateTime<Utc> = time.clone().into();
+    let date_time = dt.format("%d.%m.%Y %H:%M");
+    let json: Value = fetch_ticker();
+    println!("1 BTC = {} € ({date_time})", json["EUR"]["last"]);
+}
+
+fn fetch_ticker() -> Value {
     let url: &str = "https://blockchain.info/ticker";
     let response = match reqwest::blocking::get(url) {
         Ok(resp) => resp.text().unwrap(),
         Err(err) => panic!("Error: {}", err),
     };
 
-    let json: Value = serde_json::from_str(&response).unwrap();
-    println!("1 BTC = {} €", json["EUR"]["last"]);
+    return serde_json::from_str(&response).unwrap();
 }
